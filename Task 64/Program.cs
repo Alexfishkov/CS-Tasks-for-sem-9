1d754a75a543aa7344b29f83183dcc21e0e8d91e
Alexfishkov <afscorp@mail.ru>
2022-12-27 20:54:31 +0300

    Descending numbers

---
+++ Task 64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.WriteLine("Выведем все натуральные числа в промежутке от N до 1");
+Console.Write("Введите ваше N=>");
+int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+void NumbersDesc(int n)
+{
+    if (n == 1) Console.Write($"{n}");
+    else
+    {
+        Console.Write($"{n} ");
+        NumbersDesc(n - 1);
+    }
+
+}
+
+NumbersDesc(yourNumber);
+Console.WriteLine();
