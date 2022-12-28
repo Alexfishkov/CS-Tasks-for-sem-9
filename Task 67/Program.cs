44431319d54946f46c0b2a083893264d553e37eb
Alexfishkov <afscorp@mail.ru>
2022-12-28 20:21:27 +0300

    Sum of figures in the number

---
+++ Task 67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.Write("Введите число =>");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int FigureSum(int sum)
+{
+    if (sum == 0) return sum;
+    else
+    {
+        sum = sum % 10 + FigureSum(sum / 10);
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма его цифр составляет: {FigureSum(num)}");
